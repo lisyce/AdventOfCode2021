0c91d12f253bc812cd4241abe82425c7961f8cbf
Cara Lisy <lisyce@gmail.com>
2021-12-08 14:09:17 -0800

    day 8 encoder finds a and g

--- src/day8/Main.java
+++ src/day8/Main.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File f = new File("src/day8/input.txt");
+        File f = new File("src/day8/one_line_input.txt");
         List<String> lines = Files.readAllLines(f.toPath());
         Map<List<String>, List<String>> myNotes = new LinkedHashMap<>();
 
@@ -36,9 +36,11 @@ public class Main {
             List<String> outputs = Arrays.stream(twoHalves[1].split(" ")).collect(Collectors.toList());
             myNotes.put(inputs, outputs);
         }
+        s.close();
 
         for(List<String> key : myNotes.keySet()) {
             myNotes.replace(key ,myNotes.get(key).stream().filter(x -> !x.isEmpty()).collect(Collectors.toList()));
+
         }
 
 //        //count 1, 4, 7, and 8 (part 1)
@@ -48,5 +50,64 @@ public class Main {
 //        }
 //        System.out.println(count);
 
+        //decode
+
+        for(List<String> key : myNotes.keySet()) {
+            LinkedHashMap<String, String> decoder = new LinkedHashMap<>();
+            decoder.put("a", "");
+            decoder.put("b", "");
+            decoder.put("c", "");
+            decoder.put("d", "");
+            decoder.put("e", "");
+            decoder.put("f", "");
+            decoder.put("g", "");
+
+            //1. figure out which segments correspond to c and f (number 1)
+            List<String> cf = findSegmentByLength(key, 2);
+            //2. figure out which segments correspond to c, f, and a (number 7)
+            List<String> cfa = findSegmentByLength(key, 3);
+            //which is the unique char? that corresponds to "a"
+            for(String str : cfa) {
+                if(!cf.contains(str)) decoder.replace("a", str);
+            }
+
+            //3. which chunk corresponds to 4? (4 segments). the 2 new characters correspond to b and d but idk which
+            List<String> bcdf = findSegmentByLength(key, 4);
+            List<String> bd = new LinkedList<>();
+            for(String str : bcdf) {
+                if(!cf.contains(str)) bd.add(str);
+            }
+
+            //4. find one with six segments and all 5 of the previous jumbled values. remaining unknown is g.
+            for(String str : key) {
+                if (str.length() == 6 && str.contains(decoder.get("a")) && strContainsAllLetters(bcdf, str)) {
+                    //string has all the right characters, find the new one (g)
+                    List<String> abcdf = new LinkedList<>(bcdf);
+                    abcdf.add(decoder.get("a"));
+                    decoder.replace("g", findUniqueLetter(abcdf, str));
+                }
+
+            }
+
+        }
+
     }
+
+    public static String findUniqueLetter(List<String> nonUniques, String searchString) {
+        for(String nonUnique : nonUniques) {
+            if(searchString.contains(nonUnique)) searchString = searchString.replace(nonUnique, "");
+        }
+        return searchString;
+    }
+
+    public static boolean strContainsAllLetters(List<String> containedLetters, String searchString) {
+        for(String str : containedLetters) if(!searchString.contains(str)) return false;
+        return true;
+    }
+
+    public static List<String> findSegmentByLength(List<String> searchList, int len) {
+        for(String str : searchList) if(str.length() == len) return Arrays.stream(str.split("")).collect(Collectors.toList());
+        return new LinkedList<>();
+    }
+
 }
