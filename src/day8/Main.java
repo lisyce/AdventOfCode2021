d154d874915e6be9e2ddc61cc62b7833e1568df0
Cara Lisy <lisyce@gmail.com>
2021-12-08 16:33:03 -0800

    day 8 works. time to clean it up

--- src/day8/Main.java
+++ src/day8/Main.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File f = new File("src/day8/one_line_input.txt");
+        File f = new File("src/day8/main_input.txt");
         List<String> lines = Files.readAllLines(f.toPath());
         Map<List<String>, List<String>> myNotes = new LinkedHashMap<>();
 
@@ -50,7 +50,9 @@ public class Main {
 //        }
 //        System.out.println(count);
 
+
         //decode
+        long totalOutput = 0;
 
         for(List<String> key : myNotes.keySet()) {
             LinkedHashMap<String, String> decoder = new LinkedHashMap<>();
@@ -72,25 +74,120 @@ public class Main {
             }
 
             //3. which chunk corresponds to 4? (4 segments). the 2 new characters correspond to b and d but idk which
-            List<String> bcdf = findSegmentByLength(key, 4);
+            List<String> increasingChecker = findSegmentByLength(key, 4);
             List<String> bd = new LinkedList<>();
-            for(String str : bcdf) {
+            for(String str : increasingChecker) {
                 if(!cf.contains(str)) bd.add(str);
             }
 
             //4. find one with six segments and all 5 of the previous jumbled values. remaining unknown is g.
             for(String str : key) {
-                if (str.length() == 6 && str.contains(decoder.get("a")) && strContainsAllLetters(bcdf, str)) {
+                if (str.length() == 6 && str.contains(decoder.get("a")) && strContainsAllLetters(increasingChecker, str)) {
                     //string has all the right characters, find the new one (g)
-                    List<String> abcdf = new LinkedList<>(bcdf);
-                    abcdf.add(decoder.get("a"));
-                    decoder.replace("g", findUniqueLetter(abcdf, str));
+                    increasingChecker.add(decoder.get("a"));
+                    decoder.replace("g", findUniqueLetter(increasingChecker, str));
+                    increasingChecker.add(decoder.get("g"));
+                    break;
                 }
-
             }
 
+            //5. find a 7 segment one (#8) take out the c/f chunk and the b/d chunk and the a and g and e is for sure
+            for(String str : key) {
+                if(str.length() == 7 && strContainsAllLetters(increasingChecker, str)) {
+                    decoder.replace("e", findUniqueLetter(increasingChecker, str));
+                    increasingChecker.add(decoder.get("e"));
+                    break;
+                }
+            }
+
+            //6. find one with 6 segments and a,c,d,f,g (number 3)
+            List<String> acdfg = new LinkedList<>(List.of(
+                    cf.get(0),
+                    cf.get(1),
+                    decoder.get("g"),
+                    decoder.get("a")
+            ));
+
+            for(String str : key) {
+                if(str.length() == 5 && strContainsAllLetters(acdfg, str)) {
+                    decoder.replace("d", findUniqueLetter(acdfg, str));
+                    decoder.replace("b", bd.get(0).equals(decoder.get("d")) ? bd.get(1) : bd.get(0));
+                    break;
+                }
+            }
+
+            increasingChecker = increasingChecker.stream().filter(x -> !cf.contains(x)).collect(Collectors.toList());
+
+            //7. all that's left is c and f
+            //make sure increasingChecker has everything but c and f's corresponding values
+            List<String> finalIncreasingChecker = increasingChecker;
+            decoder.forEach((k, v) -> {
+                if(!finalIncreasingChecker.contains(v)) finalIncreasingChecker.add(v);
+            });
+
+
+            //find one that only has one remaining unknown and uses 6 segments (num 6)
+            for(String str : key) {
+                if(calculateNumUnknowns(decoder, str) == 1 && str.length() == 6) {
+                    decoder.replace("f", findUniqueLetter(increasingChecker, str));
+                    increasingChecker.add(decoder.get("f"));
+                    break;
+                }
+            }
+
+            //find a number 8 and use it to fill in c
+            for(String str : key) {
+                if(str.length() == 7) {
+                    decoder.replace("c", findUniqueLetter(increasingChecker, str));
+                    break;
+                }
+            }
+
+            //decode output
+            int output = decodeOutput(decoder, myNotes.get(key));
+            totalOutput += output;
+        }
+        System.out.println("TOTAL: " + totalOutput);
+
+    }
+
+    public static int decodeOutput(Map<String, String> decoder, List<String> outputs) {
+        StringBuilder finalString = new StringBuilder();
+        for(String str : outputs) {
+            finalString.append(decodeNumber(decoder, str));
+        }
+        return Integer.parseInt(finalString.toString());
+    }
+
+    public static String decodeNumber(Map<String, String> decoder, String decodeNumber) {
+        switch(decodeNumber.length()) {
+            case 2:
+                return "1";
+            case 3:
+                return "7";
+            case 4:
+                return "4";
+            case 5:
+                if(decodeNumber.contains(decoder.get("b"))) return "5";
+                else if(decodeNumber.contains(decoder.get("e"))) return "2";
+                return "3";
+            case 6:
+                if(!decodeNumber.contains(decoder.get("d"))) return "0";
+                else if(!decodeNumber.contains(decoder.get("c"))) return "6";
+                return "9";
+            case 7:
+                return "8";
         }
 
+        return "-1";
+    }
+
+    public static int calculateNumUnknowns(Map<String, String> decoder, String searchString) {
+        int unknowns = searchString.length();
+        for(String key : decoder.keySet()) {
+            if(searchString.contains(decoder.get(key)) && !decoder.get(key).isEmpty()) unknowns--;
+        }
+        return unknowns;
     }
 
     public static String findUniqueLetter(List<String> nonUniques, String searchString) {
