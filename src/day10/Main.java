f8269a046cf1526e7cd203eae972454fbc911763
Cara Lisy <lisyce@gmail.com>
2021-12-09 23:58:25 -0800

    day 10 done

--- src/day10/Main.java
+++ src/day10/Main.java
@@ -2,6 +2,7 @@ package day10;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -22,44 +23,68 @@ public class Main {
         if you make it to end of the line, it's incomplete but not corrupted
          */
 
-        List<String> lines = Files.readAllLines(new File("src/day10/test_input.txt").toPath());
+        List<String> lines = Files.readAllLines(new File("src/day10/main_input.txt").toPath());
         List<Character> corruptedChars = new LinkedList<>();
 
         //part 1
         for(String line : lines) {
-            Character c = lineIsCorrupted(line);
-            if(!Objects.equals(c, null)) corruptedChars.add(c);
+            List<Character> c = lineIsCorrupted(line, false);
+            if(!Objects.equals(c, null)) corruptedChars.add(c.get(0));
         }
 
         int corruptionScore = corruptedChars.stream().mapToInt(c ->
-            c == ')' ? 3 : c == ']' ? 57 : c == '}' ? 1197 : c == '>' ? 25137 : 0
+            c == ')' ? 3 : c == ']' ? 57 : c == '}' ? 1197 : 25137
         ).sum();
         System.out.println("Corruption Score: " + corruptionScore + "\n");
 
         //part 2
-        LinkedList<String> uncorruptedLines = lines.stream().filter(l -> !Objects.equals(null, lineIsCorrupted(l)))
+        LinkedList<String> incompleteLines = lines.stream().filter(l -> Objects.equals(null, lineIsCorrupted(l, false)))
                 .collect(Collectors.toCollection(LinkedList::new));
-        uncorruptedLines.forEach(System.out::println);
 
+        BigInteger[] autocompleteScores = incompleteLines.stream().map(l -> {
+            BigInteger totalScore = BigInteger.ZERO;
+            int[] closingBracketScores = lineIsCorrupted(l, true).stream()
+                    .mapToInt(c -> c == ')' ? 1 : c == ']' ? 2 : c == '}' ? 3 : 4).toArray();
+
+            for(int i : closingBracketScores) {
+                totalScore = totalScore.multiply(BigInteger.valueOf(5));
+                totalScore = totalScore.add(BigInteger.valueOf(i));
+            }
+            return totalScore;
+        }).toArray(BigInteger[]::new);
+
+
+        Arrays.sort(autocompleteScores, BigInteger::compareTo);
+        System.out.println(Arrays.toString(autocompleteScores));
+
+        BigInteger middleScore = autocompleteScores[autocompleteScores.length/2];
+        System.out.println("Middle Score: " + middleScore); //85929366 is too low
     }
 
     /**
      * checks to see if a line is corrupted
      * @param line line to check
-     * @return corrupted symbol, null if line is not corrupted (but potentially incomplete)
+     * @param autocompleteLine whether return the characters necessary to complete the string
+     * @return if not autocomplete: corrupted symbol, null if line is not corrupted (but potentially incomplete)
+     *         if autocomplete: List of chars needed to finish line
      */
-    public static Character lineIsCorrupted(String line) {
+    public static List<Character> lineIsCorrupted(String line, boolean autocompleteLine) {
         List<Character> traversingChars = new LinkedList<>();
         for(char c : line.toCharArray()) {
             //it's an opening bracket
             if(matchingBrackets.containsKey(c)) traversingChars.add(c);
             else { //it's a closing char
                 Character matchingOpeningChar = matchingBrackets.entrySet().stream().filter(x -> x.getValue().equals(c)).map(Map.Entry::getKey).findFirst().get();
-                if(traversingChars.get(traversingChars.size() - 1)  != matchingOpeningChar) return c;
+                if(traversingChars.get(traversingChars.size() - 1)  != matchingOpeningChar && !autocompleteLine) return new LinkedList<>(List.of(c));
                 traversingChars.remove(traversingChars.size()-1);
             }
         }
 
-        return null; //return null if the line is fine
+        if(!autocompleteLine) return null; //return null if the line is fine and we aren't autocompleting
+
+        //autocomplete the line
+        LinkedList<Character> autocompletedChars = new LinkedList<>();
+        traversingChars.stream().map(x -> matchingBrackets.get(x)).forEach(x -> autocompletedChars.add(0, x));
+        return autocompletedChars;
     }
 }
